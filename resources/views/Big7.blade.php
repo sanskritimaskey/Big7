44d162f7d22b6aa0d6a3faced756692a265ec3b3
sanskritimaskey <sanskritimaskey35@gmail.com>
2024-02-05 16:58:55 +0545

    first commit

---
+++ resources/views/Big7.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="shortcut icon" type="x-icon" href="/assets/img/logo.png">
+    <title>Big7 Dharan</title>
+    <style>
+      .carousel{
+        height: 550px;
+        width: 100%;
+        overflow: hidden;
+      }
+     .carousel img{
+      height: auto;
+      width: auto;
+      object-fit: cover;
+     }
+    </style>
+</head>
+<body>
+    <section>
+        <!-- top bar -->
+        <section>
+          <nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
+            <div class="container text-light">
+                <div class="w-100 d-flex justify-content-between">
+                    <div>
+                        <i class="fa-solid fa-location-dot mx-2"></i>
+                        <a class="navbar-sm-brand text-light text-decoration-none">Niketan Marg</a>
+                        <i class="fa fa-phone mx-2"></i>
+                        <a class="navbar-sm-brand text-light text-decoration-none">+977 9804373944</a>
+                    </div>
+                    <div>
+                        <a class="text-light" href="https://www.facebook.com/profile.php?id=61554153093362" target="_blank" rel="sponsored"><i class="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
+                        
+                        <a class="text-light" href="https://www.instagram.com/big7.dharan/" target="_blank"><i class="fab fa-instagram fa-sm fa-fw me-2"></i></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        </section>
+        <!-- navbar -->
+        <section>
+            <div>
+              <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="/pages/index.html">
+                    <img src="/assets/img/logo.png" class="rounded float-start" alt="Logo" width="120"></a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/pages/index.html">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Shop</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Product Categories
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#">Charger</a></li>
+                          <li><a class="dropdown-item" href="#">Earbuds</a></li>
+                          <li><a class="dropdown-item "href="#">Speaker</a></li>
+                           <li><a class="dropdown-item "href="#">Cover</a></li>
+                            <li><a class="dropdown-item "href="#">Datacable</a></li>
+                          
+
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">All Product</a></li>
+                        </ul>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="aboutus.html">About US</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            </div>
+        </section>
+        <!-- carousel -->
+        <section>
+          <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="/assets/img/UltimaAtom 820.png" class="d-block w-696 h-364" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="/assets/img/Ultima-Watch-Circle1.png" class="d-block w-466" alt="...">
+              </div>
+              <!-- <div class="carousel-item">
+                <img src="/assets/img/Ultima-Watch-Magic-Pro.png" class="d-block w-200" alt="...">
+              </div> -->
+              <div class="carousel-item">
+                <img src="/assets/img/Untitled design.png" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </section>
+
+        <!-- shop by category -->
+        <section >
+        </br>
+      </br>
+          <h2 style="text-align: center;">Shop By Category</h2>
+                      <div class="card" style="width: 18rem;">
+            
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5  class="card-title" style="text-align: center;">Product Image</h5>
+              <p class="card-text">Product Body</p>
+              <a href="$" class="btn btn-primary">Product Detail</a>
+            </div>
+            
+            <div class="card" style="width: 18rem;" >
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Product Image</h5>
+                <p class="card-text">Product Body</p>
+                <a href="$" class="btn btn-primary">Product Detail</a>
+          </div>
+      
+        </section>
+
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
